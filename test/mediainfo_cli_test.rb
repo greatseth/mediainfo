c57b17d0772d01f75f67c4de5bb7beebe7233d64
Peter Vandenberk <pvdb@neo.(none)>
2009-05-19 00:09:34 +0100

    Documenting the patch, for future reference.

--- test/mediainfo_cli_test.rb
+++ test/mediainfo_cli_test.rb
@@ -2,6 +2,23 @@ require "test_helper"
 require "mediainfo_test_helper"
 
 class MediainfoCliTest < ActiveSupport::TestCase
+
+  #
+  # Unit tests for the following "greatseth/mediainfo" patch:
+  #
+  # http://github.com/pvdb/mediainfo/commit/f49689a8366e629f4679bdd21e3071b0d452c34f
+  #
+  # ... with some help from:
+  #
+  # http://muffinresearch.co.uk/archives/2007/01/30/bash-single-quotes-inside-of-single-quoted-strings/
+  # http://notetoself.vrensk.com/2008/08/escaping-single-quotes-in-ruby-harder-than-expected/
+  #
+  # ... and documented here:
+  #
+  # problem:   http://gist.github.com/113611
+  # exception: http://gist.github.com/113614
+  # quoting:   http://gist.github.com/113629
+  #
     
   test "incorrect CLI works for filenames without parentheses, single quotes, spaces and double quotes" do
     info = mediainfo_incorrect_cli_mock "dinner.3g2"
