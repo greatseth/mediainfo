4ca80c7cd2c49539faa17f3dfae78bde5744eb25
NorseGaud <connarpierce@gmail.com>
2018-04-28 18:25:30 -0400

    v1.0.0 - Module name change to MediaInfo to match convention - ReadMe examples for - Rspecs for testing - Removal of all double quotes for single quotes, unless interpolation is needed - Removal of underscore method that isn't used - Variable name changes for readability and DRY - Hpricot is EOL, removing code to support it - Handling of characters instance_variable_set will throw an error for + moved instance_variable_set to method - Removal of some html.template and Manifest files which are not necessary for the gem to function - pull/18 (Eliminated attr_reader needing to be updated) - pull/12 (Eliminated need to add new Track/Streams) - pull/15 (URL parsing added)

--- lib/mediainfo/tracks.rb
+++ lib/mediainfo/tracks.rb
@@ -4,18 +4,26 @@ module MediaInfo
     def initialize(input = nil)
       if input && input.include?('<?xml')
         @xml = input
-        # Populate Tracks
+        @track_types = []
+
+        # Populate Streams
         case MediaInfo.xml_parser
         when 'nokogiri'
-          ::Nokogiri::XML(self.xml).xpath('//track').each { |track|
+          converted_xml = ::Nokogiri::XML(self.xml)
+          # @track_types = converted_xml.xpath('//track').map{ |track| track.attributes['type'].value }
+          converted_xml.xpath('//track').each { |track|
             attributes = Attributes.new(track.children.select{ |n| n.is_a? ::Nokogiri::XML::Element }.map{ |parameter| [parameter.name, parameter.text] })
-            track_type_name = sanitize_track_type(track.attributes.map{ |k,v| { :name => v.name, :value => v.value } })
+            track_type_name = sanitize_track_type(@track_types,track.attributes.map{ |k,v| { :name => v.name, :value => v.value } })
+            @track_types << track_type
             MediaInfo.set_singleton_method(self,track_type_name,attributes)
           }
         else # DEFAULT REXML
-          ::REXML::Document.new(self.xml).elements.each('//track') { |track|
+          converted_xml = ::REXML::Document.new(self.xml)
+          # @track_types = converted_xml.get_elements('//track').map{ |el| el['type'] }
+          converted_xml.elements.each('//track') { |track|
             track_elements = Attributes.new(track.children.select { |n| n.is_a? ::REXML::Element }.map{ |parameter| [parameter.name, parameter.text] })
-            track_type = sanitize_track_type(track.attributes.map{ |attr| { :name => attr[0], :value => attr[1] } })
+            track_type = sanitize_track_type(@track_types,track.attributes.map{ |attr| { :name => attr[0], :value => attr[1] } })
+            @track_types << track_type
             MediaInfo.set_singleton_method(self,track_type,track_elements)
           }
         end
@@ -26,12 +34,19 @@ module MediaInfo
 
     # Used for handling duplicate track types with differing streamid, etc
     # Takes an array of attributes and returns the track_name
-    def sanitize_track_type(track_attributes)
+    def sanitize_track_type(track_types,track_attributes)
       raise("Unable to sanitize a track type due to missing 'type' attribute in on of the elements: \n #{track_attributes}") if (type_attr_value = track_attributes.detect{ |attr| attr[:name] == 'type' }[:value]).nil?
-      if (streamid = track_attributes.detect{ |attr| attr[:name] == 'streamid' }).nil? # No StreamId
-        type = type_attr_value
+      if (streamid = track_attributes.detect{ |attr| attr[:name] == 'streamid' }).nil? # No StreamId, however, ensuring that if the streamid is there we use it
+        ## Even if no streamid we need to check for duplicate track names and append an integer. ONLY WORKS IF DUPLICATE TRACKS CONSECUTIVE
+        if track_types.include?(type_attr_value.downcase)
+          type = "#{type_attr_value}#{track_types.grep(/#{type_attr_value.downcase}/).count + 1}"
+        else
+          type = type_attr_value
+        end
       else
-        type = "#{type_attr_value}#{streamid[:value]}"
+        # For anything with a streamid of 1, ignore it. This is due to the logic of non-streamid duplicates not appending an integer for the first occurrence. We want to be consistent.
+        ## We use _ to separate the streamid so we can do easier matching for non-streamid duplicates
+        type = streamid[:value] == '1' ? type_attr_value : "#{type_attr_value}_#{streamid[:value]}"
       end
       return type.downcase
     end
