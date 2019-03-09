61616a88a4b4eba84eb00535b015df62bbebb280
Nathan Pierce <nathan.pierce@block.one>
2019-03-09 18:52:41 -0500

    better output for url failures + pry removal

--- spec/spec_shared_examples.rb
+++ spec/spec_shared_examples.rb
@@ -1,4 +1,5 @@
 # MediaInfo.from shared examples
+
 RSpec.shared_examples 'expected from class method for a file' do
   context 'when submitted a valid file path' do
     let(:input) { video_sample_path }
